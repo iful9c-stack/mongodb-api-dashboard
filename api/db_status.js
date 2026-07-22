088ab613ab71c996efe149d377fff6cb4e9eab63
iful9c-stack <iful9c@gmail.com>
2026-07-23 02:40:57 +0700

    Create db_status.js

---
+++ api/db_status.js
@@ -0,0 +1,35 @@
+import { getDatabase } from "./lib/mongodb.js";
+
+
+export default async function handler(req,res){
+
+    try {
+
+        const db =
+            await getDatabase();
+
+
+        const data =
+            await db
+            .collection("db_status")
+            .find({})
+            .toArray();
+
+
+        res.status(200).json({
+
+            total:data.length,
+            data
+
+        });
+
+
+    } catch(error){
+
+        res.status(500).json({
+            error:error.message
+        });
+
+    }
+
+}
